8464737ba63932b0abf4645b64a72b3168006cc9
webMasterMrBin <songlibin1994@gmail.com>
2023-11-26 10:14:49 +0800

    fix

--- src/app.js
+++ src/app.js
@@ -5,27 +5,12 @@ const app = express();
 const port = 4000;
 
 app.use(express.static(path.join(__dirname, '../public'), {
-  maxAge: 1000 * 60 * 60 * 24 * 3
+  setHeaders: (res, path, stat) => {
+    res.set('cache-control', `must-revalidate, max-age=${20}`);
+  }
 }));
 
-app.get('/about-me', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/about-me/index.html'));
-})
-
-app.get('/blog/:id', (req, res) => {
-  const { id } = req.params;
-  res.sendFile(path.join(__dirname,`../public/blog/${id}/index.html`));
-})
-
-app.get('/blog', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/blog/index.html'));
-})
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-})
-
 app.listen(port, () => {
-  console.log(`blog-backend server start in ${4000}`);
+  console.log(` server start in ${4000}`);
 })
 
