c13acd95ef69bc14213c2c5141794ee6d8da3e2f
pobing.slb <pobing.slb@@alibaba-inc.com>
2023-12-15 17:47:07 +0800

    seo验证html

--- src/app.js
+++ src/app.js
@@ -4,11 +4,13 @@ const path = require('path');
 const app = express();
 const port = 4000;
 
-app.use(express.static(path.join(__dirname, '../public'), {
+app.use(express.static(path.join(__dirname, '../public'),
+{
   setHeaders: (res, path, stat) => {
     res.set('cache-control', `must-revalidate, max-age=${60 * 60 * 24}`);
   }
-}));
+}
+));
 
 app.listen(port, () => {
   console.log('server is running on port 4000')
